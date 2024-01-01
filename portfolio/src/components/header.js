38e7954e220a45ff30196854103948ea2ee670ad
rishabh-awa <awasthirishabh52@gmail.com>
2024-01-01 21:27:18 +0530

    first commit

---
+++ portfolio/src/components/header.js
@@ -0,0 +1,16 @@
+
+import "../css/header.css"
+
+export default function Header(){
+    return(
+        <>
+        <div className="head">
+        
+        </div>
+        <div className="head2">
+            <div className="pic"></div>
+            <div className="name"></div>
+        </div>
+        </>
+    );
+}
